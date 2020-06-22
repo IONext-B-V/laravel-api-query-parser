1ac0b8ba7b27f8baec34dfd0c8a663b85d22d80a
Gydo Broos <gydo@broos.work>
2020-06-22 20:37:07 +0200

    Updated interface

--- src/Params/RequestParamsInterface.php
+++ src/Params/RequestParamsInterface.php
@@ -12,9 +12,15 @@ interface RequestParamsInterface
 
     public function getLocation(): ?LocationInterface;
 
+    public function addLocation(LocationInterface $location): void;
+
     public function hasSort(): bool;
 
     public function getSorts(): array;
+
+    public function addSort(SortInterface $sort): void;
+
+    public function addPagination(PaginationInterface $pagination): void;
 
     public function hasPagination(): bool;
 
@@ -23,4 +29,8 @@ interface RequestParamsInterface
     public function hasConnection(): bool;
 
     public function getConnections(): array;
+
+    public function addConnection(ConnectionInterface $connection): void;
+
+    public function addFilter(FilterInterface $filter): void;
 }
