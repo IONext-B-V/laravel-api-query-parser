29fe1f420fde9481389090f8669a41fd137e9269
Gydo Broos <gydo@broos.work>
2020-07-20 19:52:12 +0200

    Updated request params interface to match implementation

--- src/Params/RequestParamsInterface.php
+++ src/Params/RequestParamsInterface.php
@@ -23,4 +23,14 @@ interface RequestParamsInterface
     public function hasConnection(): bool;
 
     public function getConnections(): array;
+
+    public function addFilter(FilterInterface $filter): void;
+
+    public function addLocation(LocationInterface $location): void;
+
+    public function addSort(SortInterface $sort): void;
+
+    public function addPagination(PaginationInterface $pagination): void;
+
+    public function addConnection(ConnectionInterface $connection): void;
 }
